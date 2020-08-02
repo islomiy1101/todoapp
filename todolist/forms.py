30dd9650a19f66c48d4ff8efd1922ec8320933ed
islomiy1101 <islomiy1101@gmail.com>
2020-08-02 21:16:46 +0500

    my todo app finished

---
+++ todolist/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class TodoListForm(forms.Form):
+    text = forms.CharField(max_length=45,
+                           widget=forms.TextInput(
+                               attrs={'class': 'form-control', 'placeholder': 'Enter a username', 'aria-label': 'Todo',
+                                      'aria-describedby': 'add-btn'}
+                           ))
